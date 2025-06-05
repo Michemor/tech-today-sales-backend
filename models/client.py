b3c713b7e0eb3439328f2b71276cd15eab5f4c4f
Michemor <yatoijema@gmail.com>
2025-06-05 21:56:33 +0300

    adding modified README file

--- models/client.py
+++ models/client.py
@@ -1,2 +1,8 @@
 from database import db
-from datetime import datetine
+from datetime import datetime
+
+class Client(db.Model):
+    """
+    defines all information related to the client
+    client_id: uniquely identifies the client
+    """
